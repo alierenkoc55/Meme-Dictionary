3f1c0ab4afadfc4351b141f6fdc412cacc475cfc
alierenkoc55 <nevalilbeyi@gmail.com>
2026-09-18 21:08:42 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+meme_dict = {"CRINGE": "Garip ya da utandırıcı bir şey", "LOL": "Komik bir şeye verilen cevap","ROFL":  "Bir şakaya karşılık cevap",}
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+if word in meme_dict.keys():
+    print("Anlamı:", meme_dict[word])
+else:
+    print("Bu kelime eşleşmiyor.")
+    ask = input("Az önce yazdığınız kelimeyi bir daha yazınız.")
+    ans = input("Lütfen bu kelimenin anlamını yazınız.")
+    
+    
